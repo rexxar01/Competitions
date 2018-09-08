730cd6ebaecbfcc77290997ab0b79247b0cb82e9
TheProjectsGuy <123avneesh@gmail.com>
2018-09-08 22:33:08 +0530

    Patch fixes

--- WRO2018/Testing_Codes/arm_test_1/arm_test_1.ino
+++ WRO2018/Testing_Codes/arm_test_1/arm_test_1.ino
@@ -4,16 +4,17 @@
 
 // Polulu motors (A1-16)
 XYZrobotServo base_joint(Serial1, 3);
-XYZrobotServo randon_servo(Serial1, 1);
+XYZrobotServo random_servo(Serial1, 1);
 
 // Position coordinates of the arm
 /*
  * Joint Description
- *    - Joint base to link 1 : Polulu (ID 3)
- *    - link 1 to link 2 : Dynamixel (ID 3)
- *    - link 2 to end effector : Dynamixel (ID 1)
+ *    - Joint base to link 1 : Polulu (ID 3)          - 512
+ *    - link 1 to link 2 : Dynamixel (ID 3)           - 512
+ *    - link 2 to end effector : Dynamixel (ID 1)     - 512
  */
-int positions[] = {400, 400, 400, 400};
+bool updatePositions = false;
+int positions[] = {512, 512, 512, 512};
 void setup() {
   // Initializing code
   // Dynamixel
@@ -27,11 +28,26 @@ void setup() {
 }
 
 // Actuate arm on positions
-void actuateArmToPositios() {
-  Dynamixel.move(1, positions[3]);
-  Dynamixel.move(3, positions[2]);
-  base_joint.setPosition(positions[1], 30);
-  random_servo.setPosition(positions[0], 15);
+void actuateArmToPositios(bool debug = true) {
+  if (updatePositions) {
+    Dynamixel.move(1, positions[3]);
+    Dynamixel.move(3, positions[2]);
+    base_joint.setPosition(positions[1], 40);
+    random_servo.setPosition(positions[0], 15);
+  } else {
+    Serial.println("Arm disabled, give enable command");
+  }
+  if (debug) {
+    String msg = "Arm parameters are [";
+    int i;
+    for (i = 0; i < 4 - 1; i++) {
+      msg.concat(positions[i]);
+      msg.concat(", ");
+    }
+    msg.concat(positions[i]);
+    msg.concat("] ");
+    Serial.println(msg);
+  }
 }
 
 void loop() {
@@ -40,19 +56,29 @@ void loop() {
    * Serial command list
    * [Command][value]
    * Commands:
-   *      # -> Command mode to all the joints
+   *      A -> Position all the joints to [value]
    *      J -> Select joint with [value]
-   *          P -> Position the selected joint with [value]
+   *          P -> Position the selected joint with [value] (Use J before it in the same command)
+   *      U -> Enable positional update
+   *      D -> Disable positional update
    */
   while (Serial.available()) {
     char c = Serial.read();
-    if (c == '#') {
+    if (c == 'A') {
       int p = Serial.parseInt();
       for (int i = 0; i< 4; i++) {
         positions[i] = p;
       }
     }
-    if (c == 'J') {
+    else if (c == 'U') {
+      updatePositions = true;
+      Serial.println("Update position enabled");
+    }
+    else if (c == 'D') {
+      updatePositions = false;
+      Serial.println("Update position disabled");
+    }
+    else if (c == 'J') {
       int joint_number = Serial.parseInt();
       c = Serial.read();
       if (c == 'P') {
